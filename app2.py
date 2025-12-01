5a877c51051a1cbe8ae6e0e793c548323c09258e
Sathwika B <sathwikabamandlapally024@gmail.com>
2025-12-01 16:31:49 +0530

    voice changed

--- app2.py
+++ app2.py
@@ -15,6 +15,13 @@ app = FastAPI()
 
 def speak(text: str):
     engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate',150)
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        if "female" in voice.name.lower() or "zira" in voice.name.lower():
+            engine.setProperty('voice',voice.id)
+            break
     engine.say(text)
     engine.runAndWait()
 
